18b438b2f3b634affe2af42fd42740faafa09e93
Ard2238 <abhishekrdas28@gmail.com>
2020-11-24 06:46:01 +0530

    Added Code to add getters and setters for the remaining attributes and a constructor

--- js/Form.js
+++ js/Form.js
@@ -21,6 +21,26 @@ class Employee{
         this._startDate = startDate;
     } 
 
+    get gender() { return this._gender; }
+    set gender(gender){
+        this._gender = gender;
+    }
+
+    get department() { return this._department; }
+    set department(department){
+        this._department = department;
+    }
+
+    get salary() { return this._salary; }
+    set salary(salary){
+        this._salary = salary;
+    }
+
+    get note() { return this._note}
+    set note(note) {
+        this._note = note;
+    }
+
     toString() {
         const options = {year: 'numeric',  month: 'long', day: 'numeric'};
         const empDate = !this.startDate ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
